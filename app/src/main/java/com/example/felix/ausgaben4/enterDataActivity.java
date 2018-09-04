20895b83a39cf7d97ae500b6e674fc4d1f4a045a
fxschn <s6684276@msx.tu-dresden.de>
2018-09-04 14:58:20 +0200

    add DataPicker to enterDate and Input_typ numberDecimal to enterAmount in enterdata_activity.xml

--- app/src/main/java/com/example/felix/ausgaben4/enterDataActivity.java
+++ app/src/main/java/com/example/felix/ausgaben4/enterDataActivity.java
@@ -1,6 +1,7 @@
 package com.example.felix.ausgaben4;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,17 +10,34 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class enterDataActivity extends AppCompatActivity {
 
     private TextView enterTitle, enterDate, enterAmount;
     private EditText userEnterTitle, userEnterDate, userEnterAmount;
     private Button saveButton;
+    Calendar myCalendar = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
 
     @Override
-    public void onCreate( Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //set the right xml file
         setContentView(R.layout.enterdata_activity);
@@ -34,10 +52,13 @@ public class enterDataActivity extends AppCompatActivity {
         userEnterDate = findViewById(R.id.userEnterDate);
         userEnterAmount = findViewById(R.id.userEnterAmount);
 
-        /* TODO: need to use in MainActivity
-        final String titleToString = userEnterTitle.getText().toString();
-        final String dateToString = userEnterDate.getText().toString();
-        final String amountToString = userEnterAmount.getText().toString();*/
+
+        userEnterDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(enterDataActivity.this,date,myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
 
         saveButton = findViewById(R.id.saveData);
@@ -50,16 +71,20 @@ public class enterDataActivity extends AppCompatActivity {
                 saveData.putExtra("date", userEnterDate.getText().toString());
                 saveData.putExtra("amount", userEnterAmount.getText().toString());
                 setResult(Activity.RESULT_OK, saveData);
-
-
-
-
                 finish();
+
 
             }
         });
 
 
 
+
+    }
+
+    private void updateLabel(){
+        String myFormat = "dd/MM/yy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(myFormat, Locale.GERMAN);
+        userEnterDate.setText(simpleDateFormat.format(myCalendar.getTime()));
     }
 }
