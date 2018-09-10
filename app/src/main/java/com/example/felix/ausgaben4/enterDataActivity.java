ba018081b9bc7657a7af3c0bc6d79e7e98db071a
fxschn <s6684276@msx.tu-dresden.de>
2018-09-10 13:51:57 +0200

    change layout -fab check input in enter_data_activity

--- app/src/main/java/com/example/felix/ausgaben4/enterDataActivity.java
+++ app/src/main/java/com/example/felix/ausgaben4/enterDataActivity.java
@@ -8,11 +8,15 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -51,7 +55,7 @@ public class enterDataActivity extends AppCompatActivity {
         userEnterDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new DatePickerDialog(enterDataActivity.this,date,myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                new DatePickerDialog(enterDataActivity.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
@@ -61,25 +65,36 @@ public class enterDataActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent saveData = new Intent();
-                saveData.putExtra("title", userEnterTitle.getText().toString());
-                saveData.putExtra("date", userEnterDate.getText().toString());
-                saveData.putExtra("amount", userEnterAmount.getText().toString());
-                setResult(Activity.RESULT_OK, saveData);
-                finish();
-
+                if(checkInput()) {
+                    Intent saveData = new Intent();
+                    saveData.putExtra("title", userEnterTitle.getText().toString());
+                    saveData.putExtra("date", userEnterDate.getText().toString());
+                    saveData.putExtra("amount", userEnterAmount.getText().toString());
+                    setResult(Activity.RESULT_OK, saveData);
+                    finish();
+                }
 
             }
         });
 
 
-
-
     }
 
-    private void updateLabel(){
+    private void updateLabel() {
         String myFormat = "dd/MM/yy";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(myFormat, Locale.GERMAN);
         userEnterDate.setText(simpleDateFormat.format(myCalendar.getTime()));
+    }
+
+    //check if the user fills in all three fields
+    private boolean checkInput() {
+        if (!TextUtils.isEmpty(userEnterTitle.getText()) && !TextUtils.isEmpty(userEnterAmount.getText()) && !TextUtils.isEmpty(userEnterDate.getText())) {
+            return true;
+
+        } else {
+            Toast.makeText(this, "Bitte alle Felder ausfüllen", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
     }
 }
